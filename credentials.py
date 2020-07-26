77174ba85068d0e24e284245b02635af6e23f510
Dylan <dylan.park@att.net>
2020-07-26 02:23:33 -0500

    Added blank credentials.py file

---
+++ credentials.py
@@ -0,0 +1,26 @@
+def get_account_id():
+    return ''
+
+
+def get_client_secret():
+    return ''
+
+
+def get_client_id():
+    return ''
+
+
+def get_redirect_uri():
+    return ''
+
+
+def get_auth_url():
+    return ''
+
+
+def get_categoryid():
+    return 0
+
+
+def get_staffid():
+    return 0
